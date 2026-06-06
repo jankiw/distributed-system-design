4958a62caf579dca61568ddfd00a767ef4afd38a
Your Name <you@example.com>
2026-06-06 20:07:42 +0200

    work in progerss trying to set up sql database

--- main.tf
+++ main.tf
@@ -21,6 +21,15 @@ resource "azurerm_subnet" "example" {
     address_prefixes = var.vnet_address_prefixes
 }
 
+resource "azurerm_public_ip" "vm" {
+    count = 3
+    name = "vm-pip-${count.index}"
+    location = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
+    allocation_method = "Static"
+    sku = "Standard"
+}
+
 resource "azurerm_network_interface" "example" {
     count = 3
     name = "example-nic-${count.index}"
@@ -30,17 +39,7 @@ resource "azurerm_network_interface" "example" {
         name = "internal"
         subnet_id = azurerm_subnet.example.id 
         private_ip_address_allocation = "Dynamic"
-    }
-}
-
-resource "azurerm_storage_account" "storage" {
-    name = "mydsdstorage"
-    resource_group_name = azurerm_resource_group.example.name
-    location = azurerm_resource_group.example.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    tags = {
-        enviroment = "dev"
+        public_ip_address_id = azurerm_public_ip.vm[count.index].id
     }
 }
 
@@ -68,9 +67,42 @@ resource "azurerm_linux_virtual_machine" "example" {
     tags = {
         enviroment = "dev"
     }
-    provisioner "local-exec" {
-        command = <<EOT
-echo "STORAGE_KEY=${azurerm_storage_account.storage.primary_access_key}">> /etc/environment
-EOT
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt update",
+            "sudo apt install postgresql-client -y",
+            "echo 'export DB_HOST=${azurerm_postgresql_flexible_server.postgresql.fqdn}' >> ~/.bashrc",
+            "echo 'export DB_USER=psqladmin' >> ~/.bashrc",
+            "echo 'export DB_PASSWORD=P@sSw0rd243%' >> ~/.bashrc",
+            "source ~/.bashrc"
+        ]
     }
+}
+
+resource "azurerm_postgresql_flexible_server" "postgresql" {
+    name = "example-postgresql"
+    location = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
+    administrator_login = "psqladmin"
+    administrator_password = "P@sSw0rd243%"
+    sku_name = "GP_Standard_D2s_v3"
+    version = "13"
+    storage_mb = 32768
+    delegated_subnet_id = azurerm_subnet.example.id
+}
+
+resource "azurerm_postgresql_database" "exampledb" {
+    name = "exampledb"
+    resource_group_name = azurerm_resource_group.example.name
+    server_name = azurerm_postgresql_flexible_server.postgresql.name
+    charset = "UTFS"
+    collation = "English_United States.1252"
+}
+
+resource "azurerm_postgresql_firewall_rule" "allow_vm" {
+    name = "allow-vm"
+    resource_group_name = azurerm_resource_group.example.name
+    server_name = azurerm_postgresql_flexible_server.postgresql.name
+    start_ip_address = azurerm_public_ip.vm[0].ip_address
+    end_ip_address = azurerm_public_ip.vm[2].ip_address
 }
