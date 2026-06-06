023547bdb4299f4112e4410de94b36b4d4054eea
Your Name <you@example.com>
2026-06-06 17:11:09 +0200

    added vm and storage

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_virtual_network" "example" {
     name = "example-network"
-    address_space = ["10.0.0.0/16"]
+    address_space = var.vnet_address_space
     location = azurerm_resource_group.example.location
     resource_group_name = azurerm_resource_group.example.name
 }
@@ -18,7 +18,7 @@ resource "azurerm_subnet" "example" {
     name = "internal"
     resource_group_name = azurerm_resource_group.example.name
     virtual_network_name = azurerm_virtual_network.example.name
-    address_prefixes = ["10.0.2.0/24"]
+    address_prefixes = var.vnet_address_prefixes
 }
 
 resource "azurerm_network_interface" "example" {
@@ -30,5 +30,47 @@ resource "azurerm_network_interface" "example" {
         name = "internal"
         subnet_id = azurerm_subnet.example.id 
         private_ip_address_allocation = "Dynamic"
+    }
+}
+
+resource "azurerm_storage_account" "storage" {
+    name = "mydsdstorage"
+    resource_group_name = azurerm_resource_group.example.name
+    location = azurerm_resource_group.example.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    tags = {
+        enviroment = "dev"
+    }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+    count = 3
+    name = "example-vm-${count.index}"
+    location = azurerm_resource_group.example.location
+    resource_group_name = azurerm_resource_group.example.name
+    network_interface_ids = [azurerm_network_interface.example[count.index].id]
+    size = "Standard_B1s"
+    os_disk {
+        caching = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+    source_image_reference {
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "18.04-LTS"
+        version = "latest"
+    }
+    disable_password_authentication = false
+    admin_username = "azureuser"
+    admin_password = "P@sSw0rd243%"
+
+    tags = {
+        enviroment = "dev"
+    }
+    provisioner "local-exec" {
+        command = <<EOT
+echo "STORAGE_KEY=${azurerm_storage_account.storage.primary_access_key}">> /etc/environment
+EOT
     }
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,11 @@ variable "resource_group_name" {
 variable "location" {
     default = "polandcentral"
 }
+
+variable "vnet_address_space" {
+    default = ["10.0.0.0/16"]
+}
+
+variable "vnet_address_prefixes" {
+    default = ["10.0.2.0/24"]
+}
