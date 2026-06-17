b03c9e4f9d5ea6e8f5f780915f199b8ff6bdce03
Your Name <you@example.com>
2026-06-17 17:03:44 +0200

    t

--- infrastructure/streamlit.tf
+++ infrastructure/streamlit.tf
@@ -12,7 +12,7 @@ resource "random_id" "random_chars_web_app_name" {
 }
 
 resource "azurerm_linux_web_app" "web-app" {
-  name                = "${local.linux_web_app_name}-${random_id.random_chars_web_app_name.hex}"
+  name                = "streamlit"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   service_plan_id     = azurerm_service_plan.service_plan.id
