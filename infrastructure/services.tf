9c6ed80119bfa31905859165c98f2ee3e1daa8b4
Your Name <you@example.com>
2026-06-18 20:40:00 +0200

    r

--- infrastructure/services.tf
+++ infrastructure/services.tf
@@ -29,20 +29,53 @@ resource "azurerm_linux_web_app" "web-app" {
   }
 }
 
-resource "azurerm_linux_web_app" "notification-service" {
-  name                = "my-dsd-notifications"
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = "dsd-log-analytics"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  service_plan_id     = azurerm_service_plan.service_plan.id
-  app_settings        = merge(local.app_settings, var.app_settings)
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
 
-  site_config {
-    always_on        = true # always_on cannot be set to true when using Free, F1, D1 Sku
-    application_stack {
-      docker_image_name = var.notification_docker_image_name
-      docker_registry_url = var.docker_registry_url
-      docker_registry_username = var.use_docker_registry_auth ? var.docker_registry_username : null
-      docker_registry_password = var.use_docker_registry_auth ? var.docker_registry_password : null
+resource "azurerm_container_app_environment" "example" {
+  name                       = "Example-dsd-Environment"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+}
+
+resource "azurerm_container_app" "example" {
+  name                         = "my-dsd-notifications"
+  container_app_environment_id = azurerm_container_app_environment.example.id
+  resource_group_name          = azurerm_resource_group.example.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "examplecontainerapp"
+      image  = "ghcr.io/jankiw/distributed-system-design:notification"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+
+    env {
+      name = "SERVICE_BUS_CONNECTION_STRING"
+      value = var.bus_string
+    }
+
+    env {
+      name = "ACS_CONNECTION_STRING"
+      value = var.acs_string
+    }
+
+    env {
+      name = "ACS_SENDER_ADDRESS"
+      value = var.acs_addr
+    }
+
+    env {
+      name = "DATABASE_URI"
+      value = var.db_uri
     }
   }
 }
