3307d17cf5ce5356d465187136fe31b972f8d75a
Your Name <you@example.com>
2026-06-19 19:51:23 +0200

    bcx

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -4,6 +4,6 @@ locals {
   db_password_encoded = replace(replace(azurerm_postgresql_flexible_server.postgresql.administrator_password, "%", "%25"), "@", "%40")
   database_uri = "postgresql://${azurerm_postgresql_flexible_server.postgresql.administrator_login}:${local.db_password_encoded}@${azurerm_postgresql_flexible_server.postgresql.fqdn}:5432/${azurerm_postgresql_flexible_server_database.exampledb.name}"
   app_settings = {
-    "ORDER_API_URL" = "https://${azurerm_container_app.order.name}.${azurerm_container_app_environment.example.default_domain}"
+    ORDER_API_URL = "https://${azurerm_container_app.order.name}.${azurerm_container_app_environment.example.default_domain}"
   }
 }
