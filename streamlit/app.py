e9cfef375e7c6c9e5162b6754ed2ce70f912bff4
Your Name <you@example.com>
2026-06-19 20:10:31 +0200

    hshse

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 
-ORDER_API_URL = os.getenv("FLASK_API_URL")
+ORDER_API_URL = os.getenv("ORDER_API_URL")
 st.title("Streamlit on Azure 👋")
 st.header('Running on a Web App in a Container 🐳', divider='rainbow')
 st.write(ORDER_API_URL)
