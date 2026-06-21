a4bf6c2816acc06e2c283e36c2135ddc26752b16
Your Name <you@example.com>
2026-06-21 19:11:31 +0200

    b

--- streamlit/app.py
+++ streamlit/app.py
@@ -11,12 +11,19 @@ if not ORDER_API_URL:
 
 st.write(ORDER_API_URL)
 
-if st.button("Post item"):
+with st.form("form1"):
+    name = st.text_input("Product name")
+    amount = st.number_input("Amount")
+    price = st.number_input("Price"
+                            "")
+    post = st.form_submit_button("Post item")
+
+if post:
     endpoint = f"{ORDER_API_URL.rstrip('/')}/products"
     body = {
-        'name': 'testProduct',
-        'amount': '32',
-        'price': '137'
+        'name': name,
+        'amount': amount,
+        'price': price
     }
     try:
         response = requests.post(endpoint, json = body, timeout=10)
@@ -24,6 +31,30 @@ if st.button("Post item"):
         if response.status_code == 200:
             data = response.json()
             st.success("Product created!")
+
+        else:
+            st.error(f"API Error: Received status code {response.status_code}")
+            st.write(response.text)
+
+    except requests.exceptions.ConnectionError:
+        st.error("Could not connect to the API.")
+    except requests.exceptions.Timeout:
+        st.error("The request timed out.")
+    except Exception as e:
+        st.error(f"An unexpected error occurred: {e}")
+
+with st.form("form2"):
+    id = st.text_input("Product ID")
+    order = st.form_submit_button("Order item")
+
+if order:
+    endpoint = f"{ORDER_API_URL.rstrip('/')}/{id}"
+    try:
+        response = requests.post(endpoint, json = body, timeout=10)
+
+        if response.status_code == 200:
+            data = response.json()
+            st.success("Product ordered!")
 
         else:
             st.error(f"API Error: Received status code {response.status_code}")
